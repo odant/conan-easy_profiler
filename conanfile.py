08926ce57997af6cc6021e7a2561b7402fbbfe56
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-05-08 20:25:06 +0300

    v2.0.0

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, CMake, tools
 
 class easy_profiler_Conan(ConanFile):
     name = "easy_profiler"
-    version = "1.3.0"
+    version = "2.0.0"
     license = "MIT https://opensource.org/licenses/MIT"
     description = "Lightweight cross-platform profiler library for C++"
     url = "https://github.com/odant/conan-easy_profiler"
@@ -21,15 +21,14 @@ class easy_profiler_Conan(ConanFile):
         if self.settings.compiler.get_safe("libcxx") == "libstdc++":
             raise Exception("This package is only compatible with libstdc++11")
             
-    def source(self):
-        tools.patch(patch_file="disable-profiler_gui.patch")
-
     def build(self):
         cmake = CMake(self)
         if self.settings.compiler != "Visual Studio":
             cmake.verbose = True
         cmake.definitions["BUILD_SHARED_LIBS"] = "OFF"
         cmake.definitions["EASY_OPTION_LIB_STATIC"] = "ON"
+        cmake.definitions["EASY_OPTION_PRETTY_PRINT"] = "ON"
+        cmake.definitions["EASY_PROFILER_NO_GUI"] = "ON"
         cmake.definitions["EASY_PROFILER_NO_SAMPLES"] = "ON"
         cmake.configure()
         cmake.build()
@@ -55,7 +54,8 @@ class easy_profiler_Conan(ConanFile):
         # Defines
         self.cpp_info.defines = [
             "BUILD_WITH_EASY_PROFILER=1",
-            "EASY_PROFILER_STATIC=1"
+            "EASY_PROFILER_STATIC=1",
+            "EASY_OPTION_PRETTY_PRINT_FUNCTIONS=1",
             "EASY_OPTION_STORAGE_EXPAND_BLOCKS_ON=0",
             "EASY_OPTION_IMPLICIT_THREAD_REGISTRATION=1",
             "EASY_OPTION_BUILTIN_COLORS=1"
