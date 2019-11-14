5f55bf4a6b4c9e003a0a523880c2a8c010a9a4a6
d0nc1h0t <udincev@yandex.ru>
2019-11-14 18:06:34 +0300

    Build for VC 2017, 2019

--- build.py
+++ build.py
@@ -10,34 +10,8 @@ from conan.packager import ConanMultiPackager
 # Common settings
 username = "odant" if "CONAN_USERNAME" not in os.environ else None
 # Windows settings
-visual_versions = ["14", "15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
+visual_versions = ["15", "16"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
 visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
-visual_default_toolsets = {
-    "14": [None, "v140_xp"],
-    "15": [None, "v140_xp"]
-}
-visual_toolsets = None
-if "CONAN_VISUAL_TOOLSETS" in os.environ:
-    visual_toolsets = [s.strip() for s in os.environ["CONAN_VISUAL_TOOLSETS"].split(",")]
-
-
-def vs_get_toolsets(compiler_version):
-    return visual_toolsets if not visual_toolsets is None else visual_default_toolsets.get(compiler_version)
-
-
-def vs_add_toolset(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        toolsets = vs_get_toolsets(settings["compiler.version"])
-        if toolsets is None or settings["compiler"] != "Visual Studio":
-            result.append([settings, options, env_vars, build_requires, reference])
-        else:
-            for t in toolsets:
-                settings = deepcopy(settings)
-                settings["compiler.toolset"] = t
-                result.append([settings, options, env_vars, build_requires, reference])
-    return result
-
 
 def filter_libcxx(builds):
     result = []
@@ -46,7 +20,6 @@ def filter_libcxx(builds):
             result.append([settings, options, env_vars, build_requires, reference])
     return result
 
-    
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         username=username,
@@ -57,8 +30,6 @@ if __name__ == "__main__":
     builder.add_common_builds(pure_c=False)
     # Adjusting build configurations
     builds = builder.items
-    if platform.system() == "Windows":
-        builds = vs_add_toolset(builds)
     if platform.system() == "Linux":
         builds = filter_libcxx(builds)
     # Replace build configurations
