12edd323433e763b08dd09261f7011831f47a39c
d0nc1h0t <udincev@yandex.ru>
2025-04-28 14:15:47 +0300

    Adapted to Conan v2

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 # Dmitriy Vetutnev, Odant, 2018
 
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
 import os
 
 
@@ -12,21 +12,26 @@ class easy_profiler_Conan(ConanFile):
     license = "MIT https://opensource.org/licenses/MIT"
     description = "Lightweight cross-platform profiler library for C++"
     url = "https://github.com/odant/conan-easy_profiler"
-    settings = {
-        "os": ["Windows", "Linux"],
-        "compiler": ["Visual Studio", "gcc"],
-        "build_type": ["Release", "Debug"],
-        "arch": ["x86_64", "x86", "mips", "armv7"]
-    }
+    settings = "os", "compiler", "build_type", "arch"
     options = {
+        "ninja": [True, False],
         "stub": [False, True]
     }
-    default_options = "stub=False"
-    generators = "cmake"
-    exports_sources = "src/*", "CMakeLists.txt", "Findeasy_profiler.cmake", "disable_converter.patch", "core_install.patch"
+    default_options = {
+        "ninja": True,
+        "stub": False
+    }
+    exports_sources = "src/*", "disable_converter.patch", "core_install.patch", "fix_cmake_version.patch", "fix_atomic_init.patch"
     no_copy_source = True
     build_policy = "missing"
+    
+    def layout(self):
+        tools.cmake.cmake_layout(self, src_folder="src")
 
+    def build_requirements(self):
+        if self.options.ninja:
+            self.build_requires("ninja/[>=1.12.1]")
+            
     def configure(self):
         if self.settings.compiler.get_safe("libcxx") == "libstdc++":
             raise Exception("This package is only compatible with libstdc++11")
@@ -40,43 +45,60 @@ class easy_profiler_Conan(ConanFile):
             self.output.warn("Stub-mode, not real library!")
 
     def source(self):
-        tools.patch(patch_file="disable_converter.patch")
-        tools.patch(patch_file="core_install.patch")
+        tools.files.patch(self, patch_file="disable_converter.patch")
+        tools.files.patch(self, patch_file="core_install.patch")
+        tools.files.patch(self, patch_file="fix_cmake_version.patch")
+        tools.files.patch(self, patch_file="fix_atomic_init.patch")
+
+    def generate(self):
+        if self.options.stub:
+            return
+        benv = tools.env.VirtualBuildEnv(self)
+        benv.generate()
+        renv = tools.env.VirtualRunEnv(self)
+        renv.generate()
+        if tools.microsoft.is_msvc(self):
+            vc = tools.microsoft.VCVars(self)
+            vc.generate()
+        deps = tools.cmake.CMakeDeps(self)    
+        deps.generate()
+        cmakeGenerator = "Ninja" if self.options.ninja else None
+        tc = tools.cmake.CMakeToolchain(self, generator=cmakeGenerator)
+        tc.variables["BUILD_SHARED_LIBS"] = "ON"
+        tc.variables["EASY_OPTION_PRETTY_PRINT"] = "ON"
+        tc.variables["EASY_PROFILER_NO_GUI"] = "ON"
+        tc.variables["EASY_PROFILER_NO_SAMPLES"] = "ON"
+        tc.generate()
 
     def build(self):
         if self.options.stub:
             return
-        cmake = CMake(self)
-        if self.settings.compiler != "Visual Studio":
-            cmake.verbose = True
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
-        cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
-        cmake.definitions["EASY_OPTION_PRETTY_PRINT"] = "ON"
-        cmake.definitions["EASY_PROFILER_NO_GUI"] = "ON"
-        cmake.definitions["EASY_PROFILER_NO_SAMPLES"] = "ON"
+        cmake = tools.cmake.CMake(self)
         cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        # CMake script
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        self.copy("Findeasy_profiler.cmake", dst=".", src=".", keep_path=False)
-        # PDB
-        self.copy("easy_profiler.pdb", dst="bin", src="lib", keep_path=False)
+        if not self.options.stub:
+            cmake = tools.cmake.CMake(self)
+            cmake.install()
+            # CMake script
+            tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            # PDB
+            tools.files.copy(self, "easy_profiler.pdb", dst=os.path.join(self.package_folder, "bin"), src=os.path.join(self.build_folder, "lib"), keep_path=False)
         # Manual packing headers in stub-mode
-        if self.options.stub:
-            self.copy("*.h", dst="include", src="src/easy_profiler_core/include", keep_path=True)
-            tools.save(os.path.join(self.package_folder, "stub"), "")
+        else:
+            tools.files.copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "easy_profiler_core/include"), keep_path=True)
+            tools.files.save(os.path.join(self.package_folder, "stub"), "")
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
         if not self.options.stub:
             # Libraries
             self.cpp_info.libs = ["easy_profiler"]
             if self.settings.os == "Windows":
-                self.cpp_info.libs.extend(["ws2_32", "psapi"])
+                self.cpp_info.system_libs.extend(["ws2_32", "psapi"])
             else:
-                self.cpp_info.libs.append("pthread")
+                self.cpp_info.system_libs.append("pthread")
             # Defines
             self.cpp_info.defines = [
                 "BUILD_WITH_EASY_PROFILER=1",
